dcdaaf0af83ba684e12336ddf913d99d7fe89d85
NikolosD <alcari2002@gmail.com>
2024-04-03 20:02:58 +0400

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  // Add any additional rules or configurations here
+};
